87b0a565c74361d8576cf9494d12ab8032907cb0
Sajan261996 <sajan.kumar261996@gmail.com>
2025-05-12 15:17:43 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var userName = "Sajan kumar";
+var age = 40;
+var collage = "ITI delhi";
+console.log(userName);
+console.log(age);
+console.log(collage);
---
+++ app.ts
@@ -0,0 +1,7 @@
+var userName:string="Sajan kumar"; 
+var age=30;
+var collage:string="ITI delhi"
+
+console.log(userName);
+console.log(age);
+console.log(collage);
