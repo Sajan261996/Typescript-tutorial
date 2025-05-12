aef489a07afca1b3980b6e71c9c19bdd9303119d
Sajan261996 <sajan.kumar261996@gmail.com>
2025-05-12 15:51:31 +0530

    update commit

---
+++ number.js
@@ -0,0 +1,19 @@
+var num1 = 10;
+var num2 = 30;
+var total = num1 + num2;
+console.log(total);
+var oct = 262145;
+var binary = 1;
+var hexa = 0x00001;
+console.log(oct + 10);
+var item = 100;
+var item2 = "50";
+// var item2Converted=Number(item2);
+var item2Converted = +item2;
+// console.log(item+item2Converted);
+console.log(item + +item2);
+//
+var data = 30;
+// data=40
+// data="Sajan kumar"
+// var data: number|string
---
+++ number.ts
@@ -0,0 +1,27 @@
+var num1: number = 10;
+var num2:number = 30;
+
+var total=num1+num2;
+console.log(total);
+
+var oct:number=0o1000001;
+var binary:number=0b00001;
+var hexa:number=0x00001;
+
+console.log(oct+10)
+
+
+var item:number=100;
+var item2="50";
+// var item2Converted=Number(item2);
+var item2Converted=+item2;
+// console.log(item+item2Converted);
+console.log(item+ +item2);
+
+
+//
+var data=30
+// data=40
+// data="Sajan kumar"
+
+// var data: number|string
